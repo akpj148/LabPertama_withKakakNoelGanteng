1f3f7b4abdc821b459c6770aee29e58f7de6327b
willy <akpj148>
2026-01-14 13:21:34 +0700

    add guess words

--- WilsonWilliem_LabIntermediate25.py
+++ WilsonWilliem_LabIntermediate25.py
@@ -14,22 +14,29 @@ def randomWord():
 
 def life(lifes):
   theMan = {
-    5: '''
+    6: '''
   ____
  /  
  |
 /-\\
 ''',
-    4: '''
+    5: '''
   ____
  /   O
  |
 /-\\
 ''',
-3: '''
+4: '''
   ____
  /   O
  |   |
+/-\\
+''',
+
+3: '''
+  ____
+ /   O
+ |  /|
 /-\\
 ''',
 
@@ -57,6 +64,7 @@ def life(lifes):
 def main():
   word = randomWord()
   lengthWord = len(word)
+  wordGuess = []
   blank = "_"
   guess = []
 
@@ -65,12 +73,16 @@ def main():
 
   print("given secrete word.(\"close1\" to exit)\n type all the letter to win")
   
-  lifes = 5
+  lifes = 6
   life(lifes)
 
   while True:
     for i in range(len(guess)):
       print(guess[i]+" ", end="")
+    
+    print("words guessed: ",end="")
+    for i in range(len(wordGuess)):
+      print(wordGuess[i]+" ", end="")
     
     print("\n")
     userInput = input("guess: ").upper()
@@ -81,25 +93,27 @@ def main():
     if userInput == word:
       print("you've done it")
       break
+
+
     
     if len(userInput) > 1 or userInput in "12346890":
       print("hanya boleh 1 kata dan bukan angka")
+    elif userInput in wordGuess:
+      print(f"kata \"{userInput}\" sudah di tebak")
     elif userInput in word and userInput != "" and userInput != " ":
       print(f"terdapat {userInput} didalam kata tersebut. (\"close1\" to exit)")
       for i in range(lengthWord):
         if userInput == word[i]:
           guess[i] = userInput
     else:
+      wordGuess.append(userInput)
       print(f"tidak ada {userInput} didalam kata tersebut. (\"close1\" to exit)")
       lifes-=1
       life(lifes)
       if lifes == 0:
         print("you lose")
         break
-    
-    
-
-
+      
 main()
 
 
